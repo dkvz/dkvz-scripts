5f97aed5190b4a8d5d9905defc8788d736544e60
DkVZ <github@dkvz.eu>
2021-06-09 12:44:57 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+TARGET="${HOME}/.local/bin"
+
+read -p "Install in ${TARGET}? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo ""
+  mkdir -pv $TARGET
+  # TODO Check and warn if GeoIP DB files are absent
+  ln -sfv "$PWD/local/bin/iplookup.sh" "$TARGET/iplookup"
+  ln -sfv "$PWD/local/bin/mmdbinspect" "$TARGET/mmdbinspect"
+  ln -sfv "$PWD/local/bin/n.sh" "$TARGET/n"
+fi
