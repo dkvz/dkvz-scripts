9bf14660fb98c803a22e4abf54ec36bde6689f00
DkVZ <github@dkvz.eu>
2021-08-07 13:04:19 +0200

    Renaming basic iplookup script

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo ""
   mkdir -pv $TARGET
   # TODO Check and warn if GeoIP DB files are absent
-  ln -sfv "$PWD/local/bin/iplookup.sh" "$TARGET/iplookup"
+  ln -sfv "$PWD/local/bin/local-iplookup.sh" "$TARGET/local-iplookup"
   ln -sfv "$PWD/local/bin/mmdbinspect" "$TARGET/mmdbinspect"
   ln -sfv "$PWD/local/bin/n.sh" "$TARGET/n"
   ln -sfv "$PWD/local/bin/e.sh" "$TARGET/e"
