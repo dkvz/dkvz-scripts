b523a62fef3d8c3d8aabe3ff697a75088221048f
DkVZ <github@dkvz.eu>
2022-06-27 13:16:13 +0200

    Add option for encryption to zdir script

--- local/bin/zdir.sh
+++ local/bin/zdir.sh
@@ -4,9 +4,12 @@
 # utility is present in the path.
 
 usage() {
-  echo "$0 <DIRECTORY_TO_COMPRESS>"
+  echo "$(basename $0) [-p] <DIRECTORY_TO_COMPRESS>"
+  echo ""
   echo "Will create an archive with the same name as the source directory"
   echo "Please provide only one directory as the first argument"
+  echo ""
+  echo "The -p flag enables encryption of content and header"
   exit 0
 }
 
@@ -16,9 +19,28 @@ success() {
   echo "Created archive file: $1"
 }
 
+# Accept options "h" and "p"
+# The latter enables passwod encryption
+ARGS=""
+while getopts "hp" o; do
+  case "${o}" in
+  h)
+    usage
+    ;;
+  p)
+    ARGS="$ARGS -p -mhe=on"
+    shift
+    ;;
+  *)
+    usage
+    ;;
+  esac
+done
+#shift $((OPTIND-1))
+
 [[ ! "$#" == 1 ]] && usage
 
 [[ ! -d "$1" ]] && usage
 
 ARCHIVE_NAME=`echo "$1" | perl -p -e 's/(^.*\/)?(\w+)\/?$/$2/'`
-7z a "$ARCHIVE_NAME.7z" "$1" && success "$ARCHIVE_NAME.7z"
+7z a "$ARCHIVE_NAME.7z" $ARGS "$1" && success "$ARCHIVE_NAME.7z"
