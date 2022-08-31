35e7cc80f712211710306a4efe18fcd2194fc52e
DkVZ <github@dkvz.eu>
2022-08-31 11:00:25 +0200

    Despacito script is ready (I'm so sorry)

--- local/bin/despacito.sh
+++ local/bin/despacito.sh
@@ -33,40 +33,88 @@ verse1() {
     -f $CS5 -l 600
 }
 
+chorus() {
+  sleep .9
+  beep -f $D5 -l 600
+  sleep .3
+  beep -f $CS5 -l 600
+  sleep .2
+  beep -f $B4 -l 400 -n -f $FS4 -l 300
+  sleep .2
+  beep -f $FS4 -l 200 -n -f $FS4 -l 200 -n \
+    -f $FS4 -l 200 -n -f $FS4 -l 200 -n \
+    -f $FS4 -l 600 -n -f $FS4 -l 200 -n \
+    -f $B4 -l 400 -n -f $A4 -l 200 -n \
+    -f $B4 -l 450 -n -f $G4 -l 200
+  sleep .2
+  beep -f $G4 -l 200 -n -f $G4 -l 200 -n \
+    -f $G4 -l 200 -n -f $G4 -l 200 -n \
+    -f $B4 -l 400 -n -f $A4 -l 200 -n \
+    -f $B4 -l 400 -n -f $CS5 -l 200 -n \
+    -f $D5 -l 400 -n -f $A4 -l 200
+  sleep .15
+  beep -f $A4 -l 200 -n -f $A4 -l 200 -n \
+    -f $A4 -l 200 -n -f $A4 -l 200 -n \
+    -f $D5 -l 400 -n -f $A4 -l 200 -n \
+    -f $D5 -l 400 -n -f $E5 -l 200 -n \
+    -f $E5 -l 400 -n -f $CS5 -l 600
+}
+
+verse1
 verse1
 sleep .9
 
 # Verse 2
 beep -f $B4 -l 650; sleep .2
 beep -f $B4 -l 200 -n -f $B4 -l 200 -n \
-  -f $CS5 -l 200 -n -f $D5 -l 400 -n \
-  -f $D5 -l 400 -n -f $CS5 -l 200 -n \
-  -f $D5 -l 400 -n -f $D5 -l 400 -n \
-  -f $CS5 -l 200
-# Not super sure about that one:
+ -f $CS5 -l 200 -n -f $D5 -l 200 -n \
+ -f $D5 -l 400 -n -f $CS5 -l 200 -n \
+ -f $D5 -l 400 -n -f $CS5 -l 200
+
 beep -f $B4 -l 600; sleep .4
 beep -f $B4 -l 100 -n -f $A4 -l 100 -n \
-  -f $B4 -l 100 -n -f $CS5 -l 100 -n \
-  -f $D5 -l 400 -n -f $D5 -l 400 -n \
-  -f $CS5 -l 200 -n -f $D5 -l 400 -n \
-  -f $D5 -l 400 -n -f $E5 -l 200
+ -f $B4 -l 100 -n -f $CS5 -l 100 -n \
+ -f $D5 -l 200 -n -f $D5 -l 400 -n \
+ -f $CS5 -l 200 -n -f $D5 -l 200 -n \
+ -f $D5 -l 400 -n -f $E5 -l 200
 beep -f $A4 -l 600; sleep .2
 beep -f $A4 -l 100 -n -f $A4 -l 100 -n \
-  -f $A4 -l 100 -n -f $A4 -l 100 -n \
-  -f $D5 -l 100 -n -f $A4 -l 100 -n \
-  -f $D5 -l 100 -n -f $A4 -l 100 -n \
-  -f $D5 -l 400 -n -f $E5 -l 200 -n \
-  -f $C5 -l 600
+ -f $A4 -l 100 -n -f $A4 -l 100 -n \
+ -f $D5 -l 200 -n -f $A4 -l 200 -n \
+ -f $D5 -l 200 -n -f $A4 -l 200 -n \
+ -f $D5 -l 300 -n -f $E5 -l 200 -n \
+ -f $E5 -l 400 -n -f $CS5 -l 600
 
-sleep .2
-festival --tts <<< "Oh yeah"
-sleep .2
+# Would've been funny but disables 
+# My PC speaker for reasons I have
+# no time to investigate.
+#sleep .2
+#festival --tts <<< "Oh yeah"
+#sleep .2
 
 # Verse 3
 beep -f $B4 -l 600; sleep .4
 beep -f $B4 -l 200 -n -f $FS4 -l 200 -n \
-  -f $B4 -l 200 -n -f $DS5 -l 200 -n \
-  -f $D5 -l 400 -n -f $DS5 -l 400 -n \
-  -f $D5 -l 400 -n -f $DS5 -l 400 -n \
-  -f $D5 -l 400 -n -f $DS5 -l 200
+  -f $B4 -l 200 -n -f $CS5 -l 200 -n \
+  -f $D5 -l 400 -n -f $CS5 -l 200 -n \
+  -f $D5 -l 400 -n -f $CS5 -l 200 -n \
+  -f $D5 -l 150 -n -f $CS5 -l 100
 beep -f $B4 -l 600; sleep .4
+beep -f $B4 -l 100 -n -f $B4 -l 100 -n \
+  -f $B4 -l 100 -n -f $CS5 -l 200 -n \
+  -f $D5 -l 200 -n -f $CS5 -l 150 -n \
+  -f $D5 -l 200 -n -f $CS5 -l 200 -n \
+  -f $D5 -l 200
+sleep .2
+beep -f $E5 -l 200 -n -f $A4 -l 600
+sleep .4
+beep -f $A4 -l 100 -n -f $A4 -l 100 -n \
+  -f $A4 -l 100 -n -f $A4 -l 100 -n \
+  -f $D5 -l 200 -n -f $A4 -l 200 -n \
+  -f $D5 -l 800 -n -f $E5 -l 800 -n \
+  -f $CS5 -l 700
+
+# Chorus
+chorus
+chorus
+
