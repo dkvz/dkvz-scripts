33a836c6af3417e9b4e2bd17ca8cf14f5c3ddd5d
DkVZ <github@dkvz.eu>
2023-11-30 13:22:16 +0100

    Fix scp-old not working at all (sorry)

--- local/bin/scp-old.sh
+++ local/bin/scp-old.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copy paste of ssh-old.sh but for scp.
 
-ssh -oHostKeyAlgorithms=+ssh-rsa -oKexAlgorithms=+diffie-hellman-group1-sha1 "$@"
+scp -oHostKeyAlgorithms=+ssh-rsa -oKexAlgorithms=+diffie-hellman-group1-sha1 "$@"
