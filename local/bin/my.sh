582f4a0faa9e99ddc9d190e0f7ee73313d8965fc
DkVZ <github@dkvz.eu>
2024-08-13 21:23:30 +0200

    Add option to skip ssl for my connect to local mysql instance script

--- local/bin/my.sh
+++ local/bin/my.sh
@@ -3,4 +3,4 @@
 # Access local MySQL server in commandline (I use Docker).
 # I could couple this with some docker run / start.
 
-mysql -u root -p -h 127.0.0.1
+mysql --skip-ssl -u root -p -h 127.0.0.1
