34a7e0f33100398d7b99e97afcafcb49c60a6f87
DkVZ <github@dkvz.eu>
2025-01-04 23:23:54 +0100

    Make it possible to create a zip archive with zdir

--- local/bin/zdir.sh
+++ local/bin/zdir.sh
@@ -4,10 +4,13 @@
 # utility is present in the path.
 
 usage() {
-  echo "$(basename $0) [-p] <DIRECTORY_TO_COMPRESS>"
+  echo "$(basename $0) [-z] [-p] <DIRECTORY_TO_COMPRESS>"
   echo ""
   echo "Will create an archive with the same name as the source directory"
   echo "Please provide only one directory as the first argument"
+  echo ""
+  echo "The default format is 7z, but the -z option will create a .zip"
+  echo "archive instead."
   echo ""
   echo "The -p flag enables encryption of content and header"
   exit 0
@@ -19,28 +22,39 @@ success() {
   echo "Created archive file: $1"
 }
 
-# Accept options "h" and "p"
-# The latter enables passwod encryption
-ARGS=""
-while getopts "hp" o; do
+# Accept options "h" "p" and "z"
+MODE="7z"
+ENCRYPT=0
+while getopts "hzp" o; do
   case "${o}" in
   h)
     usage
     ;;
-  p)
-    ARGS="$ARGS -p -mhe=on"
-    shift
+  z)
+    MODE="zip"
     ;;
-  *)
+  p)
+    ENCRYPT=1
+    ;;
+  ?)
     usage
     ;;
   esac
 done
-#shift $((OPTIND-1))
+shift $((OPTIND-1))
 
 [[ ! "$#" == 1 ]] && usage
 
 [[ ! -d "$1" ]] && usage
 
 ARCHIVE_NAME=`echo "$1" | perl -p -e 's/(^.*\/)?(\w+)\/?$/$2/'`
-7z a "$ARCHIVE_NAME.7z" $ARGS "$1" && success "$ARCHIVE_NAME.7z"
+if [[ $MODE == "7z" ]]; then
+  ARGS=""
+  [[ $ENCRYPT -eq 1 ]] && ARGS="$ARGS -p -mhe=on"
+  7z a "$ARCHIVE_NAME.7z" $ARGS "$1" && success "$ARCHIVE_NAME.7z"
+else
+  # Expecting zip mode
+  ARGS="-r"
+  [[ $ENCRYPT -eq 1 ]] && ARGS="-re"
+  zip $ARGS "$ARCHIVE_NAME.zip" "$1" && success "$ARCHIVE_NAME.zip"
+fi
