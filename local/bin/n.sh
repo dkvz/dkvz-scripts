51666527f0adcb868ec6b97508b2116fdc3cb155
dkvz <github@dkvz.eu>
2025-03-30 12:16:32 +0200

    make "n v" also work instead of just "n -v"

--- local/bin/n.sh
+++ local/bin/n.sh
@@ -7,10 +7,10 @@
 DEFAULT_PATH=~/Desktop/deleteme.md
 DEFAULT_EDITOR=gedit
 DME_PATH="${DELETEME_FILE:-$DEFAULT_PATH}"
-if [ "$1" = "-v"  ]; then
+if [ "$1" = "-v"  ] || [ "$1" = "v" ]; then
   vi "$DME_PATH"
 else
   EDITOR_TO_RUN="${GUI_TEXT_EDITOR:-$DEFAULT_EDITOR}"
-  $EDITOR_TO_RUN $DME_PATH > /dev/null 2>&1 & 
+  $EDITOR_TO_RUN "$DME_PATH" > /dev/null 2>&1 & 
 fi
 
