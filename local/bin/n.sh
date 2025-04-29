10486220317b34f5289cf4c687e2cdd06323e7a4
dkvz <github@dkvz.eu>
2025-04-29 14:31:04 +0200

    allow opening my scratch deleteme file with nvim

--- local/bin/n.sh
+++ local/bin/n.sh
@@ -7,10 +7,15 @@
 DEFAULT_PATH=~/Desktop/deleteme.md
 DEFAULT_EDITOR=gedit
 DME_PATH="${DELETEME_FILE:-$DEFAULT_PATH}"
-if [ "$1" = "-v"  ] || [ "$1" = "v" ]; then
-  vi "$DME_PATH"
-else
-  EDITOR_TO_RUN="${GUI_TEXT_EDITOR:-$DEFAULT_EDITOR}"
-  $EDITOR_TO_RUN "$DME_PATH" > /dev/null 2>&1 & 
-fi
-
+case "$1" in
+  -v|v)
+    vi "$DME_PATH"
+    ;;
+  -nv|nv)
+    nvim "$DME_PATH"
+    ;;
+  *)
+    EDITOR_TO_RUN="${GUI_TEXT_EDITOR:-$DEFAULT_EDITOR}"
+    $EDITOR_TO_RUN "$DME_PATH" > /dev/null 2>&1 & 
+    ;;
+esac
