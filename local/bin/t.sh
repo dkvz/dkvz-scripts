1ba2a4ebe0f2e9b2bb3d058e72459c3e43136766
dkvz <github@dkvz.eu>
2025-05-16 10:17:03 +0200

    add argument to immediately open horizontal split in tmux

--- local/bin/t.sh
+++ local/bin/t.sh
@@ -3,9 +3,22 @@
 # Open a new tmux session
 # Unless there's at least one detached session,
 # in which case we attach the last one.
+#
+# For new sessions we have an option to open
+# a vertical split immediately using "s" or "-s"
+# as the first argument.
 
-if tmux ls > /dev/null; then
+if tmux ls > /dev/null 2>&1; then
   tmux a
 else
-  tmux
+  case "$1" in
+    -s|s)
+      tmux new-session -d
+      tmux split-window -h
+      tmux a
+      ;;
+    *)
+      tmux
+      ;;
+  esac
 fi
