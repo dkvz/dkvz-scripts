3298a25311814118879a83b8b5386a7d496de809
dkvz <github@dkvz.eu>
2025-10-04 19:28:43 +0200

    make change term bg script show previous bg

--- local/bin/change-term-bg.sh
+++ local/bin/change-term-bg.sh
@@ -8,6 +8,8 @@ BG_DIR=~/.local/share/backgrounds
 # Where to save the link (directory - Will create if missing):
 LINK_DIR=~/.local/share
 LINK_NAME="bg.jpg"
+LINK_FULLNAME="$LINK_DIR/$LINK_NAME"
+CUR_BG=$(readlink "$LINK_FULLNAME")
 
 if [ ! -d "$LINK_DIR" ]; then
   mkdir -p "$LINK_DIR"
@@ -18,6 +20,8 @@ fi
 # cares or not).
 IMG=$(find "$BG_DIR" -type f -name "*.jpg" | shuf | head -n 1)
 
+echo "Current background is:"
+echo "$CUR_BG"
 echo "Switching to $IMG"
 # Force re-create the link:
-ln -sfv "$IMG" "$LINK_DIR/$LINK_NAME"
+ln -sf "$IMG" "$LINK_FULLNAME"
