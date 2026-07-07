090b02b1b80b389f7ae62fe3b7119b8d33da6f10
dkvz <github@dkvz.eu>
2026-07-07 11:40:37 +0200

    use nvim by default in n script

--- local/bin/n.sh
+++ local/bin/n.sh
@@ -3,27 +3,30 @@
 # Opens my "deleteme" text file where I enter all kind of things that I acknowledge can be lost.
 # But not too early.
 # It's a little weird. I'm a little weird.
+# At some point default editor became neovim instead of gedit
+# and the script got uglier.
 
 DEFAULT_PATH=~/Desktop/deleteme.md
 SCRATCH_DEFAULT_PATH=~/Desktop/scratch.md
+# Actually the default graphical editor
 DEFAULT_EDITOR=gedit
 DME_PATH="${DELETEME_FILE:-$DEFAULT_PATH}"
 S_PATH="${SCRATCH_PATH:-$SCRATCH_DEFAULT_PATH}"
 case "$1" in
-  -v|v)
-    vi "$DME_PATH"
-    ;;
-  -sv|sv)
-    vi "$S_PATH"
-    ;;
-  -nv|nv)
-    nvim "$DME_PATH"
-    ;;
-  -snv|snv)
-    nvim "$S_PATH"
-    ;;
-  *)
-    EDITOR_TO_RUN="${GUI_TEXT_EDITOR:-$DEFAULT_EDITOR}"
-    $EDITOR_TO_RUN "$DME_PATH" > /dev/null 2>&1 & 
-    ;;
+-v | v)
+  vi "$DME_PATH"
+  ;;
+-sv | sv)
+  vi "$S_PATH"
+  ;;
+-g | g)
+  EDITOR_TO_RUN="${GUI_TEXT_EDITOR:-$DEFAULT_EDITOR}"
+  $EDITOR_TO_RUN "$DME_PATH" >/dev/null 2>&1 &
+  ;;
+-s | s)
+  nvim "$S_PATH"
+  ;;
+*)
+  nvim "$DME_PATH"
+  ;;
 esac
