749ee52e3f83073f19acc7848a7501e6a0540926
dkvz <github@dkvz.eu>
2026-09-03 11:42:09 +0200

    first script outline

---
+++ local/bin/rp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Scripts stands for "read page"
+# Give it a URL, will use wget to download the content
+# (wget follows redirections so it's cool)
+# Hopefuly check that it's HTML
+# Transform it into markdown
+# Open the markdown in some editor
